6f3319838ab89dc2bd997bda62ee4f75929e0627
Eugene.Zeng <eugene.zeng@gwtsz.net>
2016-12-01 10:30:41 +0800

    使用 ES6 中的 class

---
+++ src/Dog.js
@@ -0,0 +1,11 @@
+//Dog.js
+class Dog{
+    constructor(name){
+        this.name  = name;
+    }
+    
+    bark(){
+        return `Wah wah, I am ${this.name}`;
+    }
+}
+module.exports = Dog;
---
+++ src/index.js
@@ -0,0 +1,6 @@
+//index.js
+const Dog = require('Dog.js');
+
+const dog = new Dog('Toby');
+
+console.log(log.bark());
